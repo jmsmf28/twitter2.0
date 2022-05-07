58f23ec85ddca4ca024fa1298f36d5e66b93b8ae
jmsmf28 <jmsmf2011@gmail.com>
2022-05-07 09:59:50 +0100

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        twitter: '#1da1f2',
+        facebook: '#3b5998',
+        linkedin: '#0077B5',
+        github: '#24292e',
+        google: '#d34836',
+        instagram: '#e1306c',
+        pinterest: '#cb2027',
+        youtube: '#ff0000',
+        vimeo: '#1ab7ea',
+        dribbble: '#ea4c89',
+      },
+    },
   },
   plugins: [],
 }
